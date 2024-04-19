3b151086dbc80e60c2dc83194ccdc51d04a96a91
aneurysmjs <aneurysmjs@outlook.com>
2024-04-19 16:48:22 +0200

    chore(*): setup workspaces

--- app/es2015-features/let-const/let-const.ts
+++
@@ -1,6 +0,0 @@
-for (var x = 0; x <= 5; x++) {
-   // when using `var`, it doesn't respect block scope, that's why is visible outside the for loop
-   //var counter = x;
-   let counter = x;
-}
-console.log(counter); // when using `let`, it does respect block scope, and is only why visible inside for loop or whether there's braces
