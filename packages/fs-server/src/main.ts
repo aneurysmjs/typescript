1dca966d11654ae8d91d5e848eb7c7a040ba71c5
aneurysmjs <aneurysmjs@outlook.com>
2025-03-11 23:32:15 +0100

    refactor(fs-server/main): use 'readFolderHierarchy'

--- packages/fs-server/src/main.ts
+++ packages/fs-server/src/main.ts
@@ -1,47 +1,11 @@
 import express, { Express, Request, Response } from 'express';
 import { createServer, Server as HTTPServer } from 'http';
 import { WebSocketServer, WebSocket } from 'ws';
-import * as path from 'path';
-import * as fs from 'fs';
+import { readFolderHierarchy } from './application/fsCrawler.mjs';
 
 const app: Express = express();
 const httpServer: HTTPServer = createServer(app);
 const wss = new WebSocketServer({ server: httpServer });
-
-interface DirectoryStructure {
-  [key: string]: string[] | DirectoryStructure;
-}
-
-console.log('FS-server ====================');
-
-function getDirectoryStructure(dirPath: string): string[] | DirectoryStructure {
-  try {
-    const files = fs.readdirSync(dirPath);
-    const result: string[] | DirectoryStructure = [];
-
-    for (const file of files) {
-      const filePath = path.join(dirPath, file);
-      const stats = fs.statSync(filePath);
-
-      if (stats.isDirectory()) {
-        const subStructure = getDirectoryStructure(filePath);
-        if (Array.isArray(subStructure)) {
-          result.push(file);
-        } else {
-          // @ts-ignore
-          result[file] = subStructure;
-        }
-      } else {
-        result.push(file);
-      }
-    }
-
-    return result;
-  } catch (error) {
-    console.error(`Error reading directory ${dirPath}:`, error);
-    return [];
-  }
-}
 
 wss.on('connection', (ws: WebSocket) => {
   console.log('Client connected');
@@ -52,7 +16,7 @@ wss.on('connection', (ws: WebSocket) => {
 
       if (parsedMessage.action === 'directories') {
         const rootDir = process.cwd(); // Current working directory
-        const structure = getDirectoryStructure(rootDir);
+        const structure = readFolderHierarchy(rootDir);
         ws.send(JSON.stringify({ type: 'directories', data: structure }));
       }
     } catch (error) {
